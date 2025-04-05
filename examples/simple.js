6d0207736c3de5f5b24f524f01fc88d9e0373a5f
himanshu-sheetlani <himanshu.sheetlani2909@gmail.com>
2025-04-05 23:50:19 +0530

    removed old

--- examples/simple.js
+++
@@ -1,2 +0,0 @@
-console.log('API Server starting');
-const express = require('express'); const app = express(); app.get('/api/health', (req, res) => res.json({ status: 'ok' })); app.listen(3000, () => console.log('Server running on port 3000'));
